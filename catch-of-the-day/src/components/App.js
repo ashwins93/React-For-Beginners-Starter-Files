9267813c68a04b74d33fa79d3839e60469cfce0f
Ashwin <aswinjeganath@gmail.com>
2018-10-15 01:00:08 +0530

    End of lecture 22

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -42,6 +42,18 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  updateFish = (key, updatedFish) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  };
+
+  deleteFish = key => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = null;
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({
       fishes: sampleFishes,
@@ -51,6 +63,12 @@ class App extends React.Component {
   addToOrder = key => {
     const order = { ...this.state.order };
     order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
+  deleteFromOrder = key => {
+    const order = { ...this.state.order };
+    delete order[key];
     this.setState({ order });
   };
 
@@ -69,10 +87,17 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          deleteFromOrder={this.deleteFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 import { formatPrice } from '../helpers';
 
 class Order extends Component {
@@ -8,16 +10,42 @@ class Order extends Component {
     const count = this.props.order[key];
     if (fish.status !== 'available') {
       return (
-        <li key={key}>
-          Sorry {fish ? fish.name : 'fish'} is no longer available
-        </li>
+        <CSSTransition
+          key={key}
+          classNames="order"
+          timeout={{ enter: 500, exit: 500 }}
+        >
+          <li key={key}>
+            Sorry {fish ? fish.name : 'fish'} is no longer available
+          </li>
+        </CSSTransition>
       );
     }
     return (
-      <li key={key}>
-        {count} lbs {fish.name}
-        {formatPrice(count * fish.price)}
-      </li>
+      <CSSTransition
+        key={key}
+        classNames="order"
+        timeout={{ enter: 250, exit: 300 }}
+      >
+        <li key={key}>
+          <span>
+            <TransitionGroup className="count" component="span">
+              <CSSTransition
+                classNames="count"
+                timeout={{ enter: 500, exit: 500 }}
+                key={count}
+              >
+                <span>{count}</span>
+              </CSSTransition>
+            </TransitionGroup>
+            lbs {fish.name}
+            {formatPrice(count * fish.price)}
+          </span>
+          <button onClick={() => this.props.deleteFromOrder(key)}>
+            &times;
+          </button>
+        </li>
+      </CSSTransition>
     );
   };
 
@@ -33,7 +61,9 @@ class Order extends Component {
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <TransitionGroup component="ul" className="order">
+          {orderIds.map(this.renderOrder)}
+        </TransitionGroup>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
